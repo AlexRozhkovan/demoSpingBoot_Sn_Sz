6d32dba37c2c10f1146a95131ab106b688ce556c
Aleksandr <zholud.aleksandr@gmail.com>
2021-08-15 17:48:40 +0300

    Initial commit

---
+++ src/main/java/com/example/demospingboot_snsz/DemoSpingBootSnSzApplication.java
@@ -0,0 +1,15 @@
+package com.example.demospingboot_snsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpingBootSnSzApplication
+{
+    
+    public static void main( String[] args )
+    {
+        SpringApplication.run( DemoSpingBootSnSzApplication.class , args );
+    }
+    
+}
---
+++ src/test/java/com/example/demospingboot_snsz/DemoSpingBootSnSzApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demospingboot_snsz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpingBootSnSzApplicationTests
+{
+    
+    @Test
+    void contextLoads()
+    {
+    }
+    
+}
