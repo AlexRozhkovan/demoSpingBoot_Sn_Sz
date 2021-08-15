c7d666c3cc3faa801e5ae4dcc7ab786ce5a5edc1
Aleksandr <zholud.aleksandr@gmail.com>
2021-08-15 18:15:19 +0300

    initial commit

---
+++ src/main/java/com/example/demospingboot_snsz/domain/Employee.java
@@ -0,0 +1,73 @@
+package com.example.demospingboot_snsz.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String country;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_fk")
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+}
---
+++ src/main/java/com/example/demospingboot_snsz/web/EmployeeRestController.java
@@ -0,0 +1,87 @@
+package com.example.demospingboot_snsz.web;
+
+import com.example.demospingboot_snsz.domain.Employee;
+import com.example.demospingboot_snsz.domain.EmployeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class EmployeeRestController {
+
+    private final EmployeeRepository repository;
+
+    public EmployeeRestController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+    
+    //Операция сохранения юзера в базу данных
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee saveEmployee(@RequestBody Employee employee) {
+
+        return repository.save(employee);
+    }
+
+    //Получение списка юзеров
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getAllUsers() {
+
+        return repository.findAll();
+    }
+
+    //Получения юзера по id
+    @GetMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee getEmployeeById(@PathVariable long id) {
+
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id = " + id));
+
+        if (employee.getIsDeleted()) {
+            throw new EntityNotFoundException("Employee was deleted with id = " + id);
+        }
+
+        return employee;
+    }
+
+    //Обновление юзера
+    @PutMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee refreshEmployee(@PathVariable("id") long id, @RequestBody Employee employee) {
+
+        return repository.findById(id)
+                .map(entity -> {
+                    entity.setName(employee.getName());
+                    entity.setEmail(employee.getEmail());
+                    entity.setCountry(employee.getCountry());
+                    return repository.save(entity);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id = " + id));
+    }
+
+    //Удаление по id
+    //@DeleteMapping("/users/{id}")
+    @PatchMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeEmployeeById(@PathVariable long id) {
+        repository.findById(id)
+                .map(employee -> {
+                    employee.setIsDeleted(Boolean.TRUE);
+                    return repository.save(employee);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Employee not found with id = " + id));
+    }
+
+    //Удаление всех юзеров
+    @DeleteMapping("/users")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeAllUsers() {
+        repository.deleteAll();
+    }
+}
