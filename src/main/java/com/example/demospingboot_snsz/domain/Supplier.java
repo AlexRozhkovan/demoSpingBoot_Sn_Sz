0646556403c11b6cf264c2381a5e43bcdf2241c0
Aleksandr <zholud.aleksandr@gmail.com>
2021-08-15 22:46:59 +0300

    ++

--- src/main/java/com/example/demospingboot_snsz/domain/Supplier.java
+++ src/main/java/com/example/demospingboot_snsz/domain/Supplier.java
@@ -5,19 +5,21 @@ import java.util.Collection;
 import java.util.Set;
 
 @Entity
-@Table(name = "SUPPLIERS")
+@Table( name = "SUPPLIERS" )
 public class Supplier
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue( strategy = GenerationType.SEQUENCE )
     private long id;
-
+    
     private String name;
-
-    @Column(name = "is_deleted")
+    
+    @Column( name = "is_deleted" )
     private Boolean isDeleted = Boolean.FALSE;
     
-    @OneToMany(mappedBy="supplier", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany( mappedBy = "supplier",
+                cascade = CascadeType.ALL,
+                fetch = FetchType.EAGER )
     private Collection <Person> contactperson_fk;
     
     public Collection <Person> getContactperson_fk()
@@ -30,39 +32,47 @@ public class Supplier
         this.contactperson_fk = contactperson_fk;
     }
     
-    @OneToOne( cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_fk")
+    @OneToOne( cascade = CascadeType.ALL )
+    @JoinColumn( name = "address_fk" )
     private Address address;
     
-    public Address getAddress() {
+    public Address getAddress()
+    {
         return address;
     }
-
-    public void setAddress(Address address) {
+    
+    public void setAddress( Address address )
+    {
         this.address = address;
     }
-
-    public Long getId() {
+    
+    public Long getId()
+    {
         return id;
     }
-
-    public void setId(Long id) {
+    
+    public void setId( Long id )
+    {
         this.id = id;
     }
-
-    public String getName() {
+    
+    public String getName()
+    {
         return name;
     }
-
-    public void setName(String name) {
+    
+    public void setName( String name )
+    {
         this.name = name;
     }
-
-    public Boolean getIsDeleted() {
+    
+    public Boolean getIsDeleted()
+    {
         return isDeleted;
     }
-
-    public void setIsDeleted(Boolean deleted) {
+    
+    public void setIsDeleted( Boolean deleted )
+    {
         isDeleted = deleted;
     }
 }
